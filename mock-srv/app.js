1b3beb2831425aad78165475bd024ffc98328b8f
Bruno-Tr-Lima <105646884+Bruno-Tr-Lima@users.noreply.github.com>
2024-02-26 19:54:47 -0300

    Mocking GET Routes with Fastify

--- mock-srv/app.js
+++ mock-srv/app.js
@@ -1,16 +1,17 @@
-import path from 'path'
-import AutoLoad from '@fastify/autoload'
-import { fileURLToPath } from 'url'
+import path from 'node:path';
+import AutoLoad from '@fastify/autoload';
+import { fileURLToPath } from 'node:url';
+import cors from '@fastify/cors';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Pass --options via CLI arguments in command to enable these options.
-export const options = {}
+export const options = {};
 
 export default async function (fastify, opts) {
   // Place here your custom code!
-
+  fastify.register(cors);
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
@@ -19,12 +20,12 @@ export default async function (fastify, opts) {
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
-  })
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'routes'),
     options: Object.assign({}, opts)
-  })
+  });
 }
